27ad3c9ae6279b9d8dcc1613b449d05b5c4bbb79
matthewobrien <matthew.kevin.obrien@gmail.com>
2021-04-09 15:37:25 -0400

    basic functionality complete will attempt stretch goals next commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
+
+let form = document.querySelector("#create-task-form")
+let taskBox = document.querySelector("#new-task-description")
+let newTask = document.querySelector("#list")
+
+form.addEventListener("submit", function(e){
+  e.preventDefault()
+  let createNewTask = document.createElement("li")
+  createNewTask.innerText = taskBox.value
+  newTask.append(createNewTask)
+
+
+})
